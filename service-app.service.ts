197397976ce035b30296dd00679e252a3fe99f8c
Aida <85931290+alievnasg@users.noreply.github.com>
2022-10-16 19:43:58 +0600

    Add files via upload

---
+++ service-app.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+export interface Config {
+  heroesUrl: string;
+  textfile: string;
+  date: any;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceAppService {
+
+  constructor(private http: HttpClient) { }
+
+
+}
